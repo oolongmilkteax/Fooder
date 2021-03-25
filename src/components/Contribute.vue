f3df6bf38378867e979b90c348812741c23efe6a
Imbabot7 <kennethkhoo98@gmail.com>
2021-03-25 16:22:52 +0800

    Contribution for restaurant

--- src/components/Contribute.vue
+++ src/components/Contribute.vue
@@ -19,20 +19,82 @@
         <button type="button" v-on:click="Recipe">Recipe</button>
         <br />
         <div v-show="contributeType === 'restaurant'">
-          <label for="restName">Restaurant Name:</label>
-          <br />
-          <input class="contriInput" type="text" id="restName2" name="restName" />
-          <br />
+          <label for="restAddress">Restaurant Address:</label><br>
+            <input
+            class="contriInput"
+            type="text"
+            id="restAddress"
+            name="restAddress"
+            v-model="restAddress"
+            ><br>
 
-          <label for="restAddress">Restaurant Address:</label>
-          <br />
-          <input class="contriInput" type="text" id="restAddress" name="restAddress" />
-          <br />
+            <p class="errorMsgs" v-show="this.missingRestAddress">Please enter the restaurant's address</p>
 
-          <label for="openingHours">Opening hours:</label>
-          <br />
-          <input class="contriInput" type="text" id="openingHours" name="openingHours" />
-          <br />
+            <label for="restCuisine">Cuisine:</label><br>
+            <input
+            class="contriInput"
+            type="text"
+            id="restCuisine"
+            name="restCuisine"
+            v-model="restCuisine"
+            ><br>
+
+            <p class="errorMsgs" v-show="this.missingRestCuisine">Please enter the restaurant's cuisine</p>
+
+            <label for="imageLink">Image:</label><br>
+            <input
+            class="contriInput"
+            type="text"
+            id="imageLink"
+            name="imageLink"
+            v-model="imageLink" placeholder="Please provide a valid link"
+            ><br>
+
+            <p class="errorMsgs" v-show="this.missingImageLink">Please submit an image of the restaurant</p>
+
+            <label for="restName">Restaurant Name:</label><br>
+            <input
+            class="contriInput"
+            type="text"
+            id="restName2"
+            name="restName"
+            v-model="restName"
+            ><br>
+
+            <p class="errorMsgs" v-show="this.missingRestName">Please enter the name of the restaurant</p>
+
+            <label for="openingHours">Opening hours:</label><br>
+            <input
+            class="contriInput"
+            type="text"
+            id="openingHours"
+            name="openingHours"
+            v-model="openingHours" placeholder="eg 11:30am–2:30pm, 5:30pm–10:30pm"
+            ><br>
+
+            <p class="errorMsgs" v-show="this.missingOpeningHours">Please state the time periods that the restaurant will be open for</p>
+
+            <label for="priceRange">Price range:</label><br>
+            <input
+            class="contriInput"
+            type="text"
+            id="priceRange"
+            name="priceRange"
+            v-model="priceRange" placeholder="eg $15–$35"
+            ><br>
+
+            <p class="errorMsgs" v-show="this.missingPriceRange">Please kindly estimate how much a meal would cost at this restaurant</p>
+
+            <label for="websiteLink">Website:</label><br>
+            <input
+            class="contriInput"
+            type="text"
+            id="websiteLink"
+            name="websiteLink"
+            v-model="websiteLink"
+            ><br>
+
+            <p class="errorMsgs" v-show="this.missingWebsiteLink">Please enter the restaurant's website</p>
         </div>
 
         <div v-show="contributeType === 'recipe'">
@@ -46,7 +108,7 @@
           <textarea class="contriInput" type="text" id="ingre" name="fullName"></textarea>
           <br />
         </div>
-        <button id="submitContributionButton">Submit!</button>
+        <button id="submitContributionButton" type="button" v-on:click="addRestaurant(); addContribution()">Submit!</button>
       </form>
       <div class="footerContainer">
         <p class="footerText">Design by JKJR</p>
@@ -56,10 +118,30 @@
 </template>
 
 <script>
+import firebase from "../firebase.js"
+var db = firebase.firestore()
+import {getUid} from "../userObj.js"
+
 export default {
   data() {
     return {
-      contributeType: "nothing"
+      contributeType: "nothing", 
+      missingRestAddress: false,
+      missingRestCuisine: false,
+      missingImageLink: false,
+      missingRestName: false,
+      missingOpeningHours: false,
+      missingPriceRange: false,
+      missingWebsiteLink: false,
+      restAddress: "",
+      restCuisine: "",
+      imageLink: "",
+      restName: "",
+      openingHours: "",
+      priceRange: "",
+      websiteLink: "",
+      allFilled: true,
+      size: 0
     };
   },
   methods: {
@@ -68,7 +150,99 @@ export default {
     },
     Recipe: function() {
       this.contributeType = "recipe";
-    }
+    },
+    addRestaurant: function(){
+
+      if (this.restAddress === "") {
+          this.missingRestAddress = true;
+          this.allFilled = false;
+      } else {
+          this.missingRestAddress = false;
+          this.allFilled = true;
+      }
+
+      if (this.restCuisine === "") {
+          this.missingRestCuisine = true;
+          this.allFilled = false;
+      } else {
+          this.missingRestCuisine = false;
+          this.allFilled = true;
+      }
+
+      if (this.imageLink === "") {
+          this.missingImageLink = true;
+          this.allFilled = false;
+      } else {
+          this.missingImageLink = false;
+          this.allFilled = true;
+      }
+
+      if (this.restName === "") {
+          this.missingRestName = true;
+          this.allFilled = false;
+      } else {
+          this.missingRestName = false;
+          this.allFilled = true;
+      }
+
+      if (this.openingHours === "") {
+          this.missingOpeningHours = true;
+          this.allFilled = false;
+      } else {
+          this.missingOpeningHours = false;
+          this.allFilled = true;
+      }
+
+      if (this.priceRange === "") {
+          this.missingPriceRange = true;
+          this.allFilled = false;
+      } else {
+          this.missingPriceRange = false;
+          this.allFilled = true;
+      }
+      /*
+      if (this.websiteLink === "") {
+          this.missingWebsiteLink = true;
+          this.allFilled = false;
+      } else {
+          this.missingWebsiteLink = false;
+          this.allFilled = true;
+      }
+      */
+
+      if (this.allFilled == true) {
+        var newId = (this.size+1).toString();
+        db.collection('restaurant').doc(newId).set({
+            address: this.restAddress,
+            cuisine: this.restCuisine,
+            image: this.imageLink,
+            name: this.restName,
+            openingHours: this.openingHours,
+            priceRange: this.priceRange,
+            websiteLink: this.websiteLink
+        }).then(() => {location.reload()});  
+
+        //db.collection("restaurant").doc("21").delete()
+      }
+    },
+    addContribution: function() {
+    
+        if(this.allFilled==true) {
+          var docRef = db.collection('restaurant').doc((this.size+1).toString());
+          
+          db.collection('user').doc(getUid()).update({
+            contributeRestaurant: firebase.firestore.FieldValue.arrayUnion(docRef)
+          }).then(() => {location.reload()});  
+        }
+
+        db.collection("restaurant").doc("25").delete()
+
+    }             
+  },
+  created() {
+    db.collection('restaurant').get().then(snap => {
+        this.size = snap.size; 
+    });
   }
 };
 </script>
@@ -114,4 +288,10 @@ export default {
   border-radius: 8px;
   text-decoration: none;
 }
+
+.errorMsgs {
+  text-align: left;
+  color: red;
+}
+
 </style>
