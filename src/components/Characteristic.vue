989846c0fd22e2fe143d5087f0dbf6c20fd0be3f
Jasmine Chen <jasminechenyuqinggg@gmail.com>
2021-04-06 22:49:44 +0800

    cannot access characteristics after first log in

--- src/components/Characteristic.vue
+++ src/components/Characteristic.vue
@@ -7,16 +7,10 @@
       <router-link to="/preferencing" class="routes">Preferencing</router-link>
       <router-link to="/restaurant" class="routes">Restaurant</router-link>
       <router-link to="/searchpage" class="routes">Search Page</router-link>
-      <router-link @click.native="logout" to="/" class="routes"
-        >Logout</router-link
-      >
+      <router-link @click.native="logout" to="/" class="routes">Logout</router-link>
       <router-link to="/profile" class="routes">Profile</router-link>
-      <router-link to="/characteristic" class="routes"
-        >Characteristic</router-link
-      >
-      <router-link to="/profileresults" class="routes"
-        >ProfileSearch</router-link
-      >
+      <router-link to="/characteristic" class="routes">Characteristic</router-link>
+      <router-link to="/profileresults" class="routes">ProfileSearch</router-link>
     </ul>
     <div v-if="isLoaded">
       <radar-chart :parsedData="parsedData"></radar-chart>
@@ -28,12 +22,12 @@
     </div>
     <li class="characteristicslist" v-for="item in recommendedThings" v-bind:key="item.id">
       <div class="pointDiv">
-        {{item.name}} 
+        {{item.name}}
         <img v-bind:src="item.image" alt="Food image" />
         <button class="myButton smaller" v-on:click="go(item)">Go!</button>
-      </div> 
+      </div>
     </li>
-    <b-button href="/searchpage" class = "gotoSearchPage">Find your own Recipes and Restaurants!</b-button>
+    <b-button href="/searchpage" class="gotoSearchPage">Find your own Recipes and Restaurants!</b-button>
     <div class="footerContainer">
       <p class="footerText">Design by JKJR</p>
     </div>
@@ -41,68 +35,79 @@
 </template>
 
 <script>
-import firebase from '../firebase.js'
-var db = firebase.firestore()
-import RadarChart from './RadarChart.js';
+import firebase from "../firebase.js";
+var db = firebase.firestore();
+import RadarChart from "./RadarChart.js";
 import logout from "./logout.js";
 export default {
   components: {
-    RadarChart,
+    RadarChart
   },
   data() {
     return {
-      parsedData:[],
-      cuisine:"",
-      type:"",
+      parsedData: [],
+      cuisine: "",
+      type: "",
       recommendedThings: [],
-      preferences:[],
-      isLoaded:false,
+      preferences: [],
+      isLoaded: false
     };
   },
   methods: {
+    notFirstLogin: function() {
+      this.$store.commit("setFirstLogin", false);
+    },
     logout: logout,
     typeFinder: function() {
       if (this.preferences[1] > 20 && this.preferences[2] >= 15) {
         this.type = "Adventurous Foodie";
-        db.collection("restaurant").orderBy('name').limit(3)
-        .get()
-        .then(snapshot => {
-          snapshot.docs.forEach(doc => {
+        db.collection("restaurant")
+          .orderBy("name")
+          .limit(3)
+          .get()
+          .then(snapshot => {
+            snapshot.docs.forEach(doc => {
               this.recommendedThings.push(doc.data());
+            });
           });
-        });
-      }else if(this.preferences[0] > 20 && this.preferences[3]>=15){
+      } else if (this.preferences[0] > 20 && this.preferences[3] >= 15) {
         this.type = "Home Cook";
-        db.collection("recipe").orderBy('name').limit(3)
-        .get()
-        .then(snapshot => {
-          snapshot.docs.forEach(doc => {
+        db.collection("recipe")
+          .orderBy("name")
+          .limit(3)
+          .get()
+          .then(snapshot => {
+            snapshot.docs.forEach(doc => {
               this.recommendedThings.push(doc.data());
+            });
           });
-        });
-      }else{
+      } else {
         this.type = "Not-So-Picky Eater";
-        db.collection("recipe").orderBy('name').limit(2)
-        .get()
-        .then(snapshot => {
-          snapshot.docs.forEach(doc => {
+        db.collection("recipe")
+          .orderBy("name")
+          .limit(2)
+          .get()
+          .then(snapshot => {
+            snapshot.docs.forEach(doc => {
               this.recommendedThings.push(doc.data());
+            });
           });
-        });
-        db.collection("restaurant").orderBy('name').limit(2)
-        .get()
-        .then(snapshot => {
-          snapshot.docs.forEach(doc => {
+        db.collection("restaurant")
+          .orderBy("name")
+          .limit(2)
+          .get()
+          .then(snapshot => {
+            snapshot.docs.forEach(doc => {
               this.recommendedThings.push(doc.data());
+            });
           });
-        });
       }
     },
-    go:function(item){
-      if(item.address != null){
+    go: function(item) {
+      if (item.address != null) {
         this.goRestaurant(item.websiteLink);
-      }else{
-        this.goRecipe(item.ingredients,item.directions);
+      } else {
+        this.goRecipe(item.ingredients, item.directions);
       }
     },
     goRecipe: function(ingredients, directions) {
@@ -113,22 +118,23 @@ export default {
     },
     goRestaurant: function(url) {
       window.open(url);
-    },
+    }
   },
   created: function() {
     db.collection("user")
-        .doc(this.$store.state.uid)
-        .get()
-        .then((doc) => {
-          this.preferences = doc.data().preferences;
-          if (this.preferences.length == 5) {
-            this.cuisine = this.preferences.pop();
-            this.parsedData = this.preferences;
-          }
-          this.isLoaded = true;
-          this.typeFinder();
-    }); 
-  },
+      .doc(this.$store.state.uid)
+      .get()
+      .then(doc => {
+        this.preferences = doc.data().preferences;
+        if (this.preferences.length == 5) {
+          this.cuisine = this.preferences.pop();
+          this.parsedData = this.preferences;
+        }
+        this.isLoaded = true;
+        this.typeFinder();
+      });
+    window.addEventListener("beforeunload", this.notFirstLogin);
+  }
 };
 </script>
 
@@ -144,21 +150,20 @@ export default {
   list-style-type: none;
 }
 
-.pointDiv{
+.pointDiv {
   flex-direction: column;
   display: flex;
   align-items: center;
 }
 
-.gotoSearchPage{
-    display: block;
-    margin-left: auto;
-    margin-right: auto;
-    width: 20%;
+.gotoSearchPage {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 20%;
 }
 
-.smaller{
+.smaller {
   width: 25%;
 }
-
 </style>
